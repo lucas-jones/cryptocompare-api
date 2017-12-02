f7e21339f232d8f3ff57de743a21bd81b5f04dc9
Robert Honsby <rhonsby@gmail.com>
2017-12-02 14:17:23 -0800

    Extracts non-api methods into a helper file

---
+++ src/helpers.ts
@@ -0,0 +1,40 @@
+import {
+    QueryParamsObject,
+} from './interfaces';
+
+/**
+ * Gets data from the CryptoCompare api.
+ */
+export const request = (path: string, options: QueryParamsObject = {}): Promise<any> => {
+    const queryString = convertObjectToQueryString(options);
+    const url = `https://min-api.cryptocompare.com/data/${path}${queryString}`;
+
+    return fetch(url)
+        .then(res => res.json())
+        .then(body => {
+            if (body.Response === 'Error') {
+                throw new Error(body.Message);
+            }
+
+            return body;
+        });
+};
+
+/**
+ * Takes an object and returns a query string
+ */
+export const convertObjectToQueryString = (obj: QueryParamsObject) => {
+    const queryParamPairs: string[] = [];
+
+    Object.keys(obj).forEach(key => {
+        const value = obj[key];
+
+        if (value !== undefined) {
+            queryParamPairs.push(`${key}=${value}`);
+        }
+    });
+
+    const queryString = queryParamPairs.join('&');
+
+    return queryString.length ? `?${queryString}` : queryString;
+};
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,11 @@
+import { request } from './helpers';
+
 import {
     CoinListResponse,
     PriceOptions,
     PriceResponse,
     QueryParamsObject,
 } from './interfaces';
-
-/**
- * Gets data from the CryptoCompare api.
- */
-export const request = (path: string, options: QueryParamsObject = {}): Promise<any> => {
-    const queryString = convertObjectToQueryString(options);
-    const url = `https://min-api.cryptocompare.com/data/${path}${queryString}`;
-
-    return fetch(url)
-        .then(res => res.json())
-        .then(body => {
-            if (body.Response === 'Error') {
-                throw new Error(body.Message);
-            }
-
-            return body;
-        });
-};
 
 /**
  * Get general info for all the coins available on the website.
@@ -106,22 +90,3 @@ export const getSocialStats = () => {};
  * The number of pairs you get is the minimum of the limit you set (default 5) and the total number of pairs available.
  */
 export const getTopPairs = () => {};
-
-/**
- * Takes an object and returns a query string
- */
-export const convertObjectToQueryString = (obj: QueryParamsObject) => {
-    const queryParamPairs: string[] = [];
-
-    Object.keys(obj).forEach(key => {
-        const value = obj[key];
-
-        if (value !== undefined) {
-            queryParamPairs.push(`${key}=${value}`);
-        }
-    });
-
-    const queryString = queryParamPairs.join('&');
-
-    return queryString.length ? `?${queryString}` : queryString;
-};
